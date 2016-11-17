efe694bda7b16842ae3321da3afdd5ab221708e7
Carlos Andres Frutos Amador <amador0180@gmail.com>
2016-11-17 01:05:28 -0600

    Subida de imagenes y Visitantes
    Imagenes de Categoria de Productos que se mostraran al publico

---
+++ productos.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="UTF8">
+  <title>Productos</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+  <link href="css/index_style.css" rel="stylesheet">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="css/estilos.css">
+  <link rel="stylesheet" type="text/css" href="css/ihover.min.css">
+</head>
+<body>
+    <header>
+      <div>
+    	<img  class="logo img-responsive" src="img/titulo4.png" align="center" width="500px" height="100px" hspace="450px">
+      </div> 
+      <div class="container"> 
+    	<div class="navbar-header"> 
+      		<button aria-controls="bs-navbar" aria-expanded="false" class="navbar-toggle collapsed" data-target="#bs-navbar" data-toggle="collapse" type="button"> 
+        		<img src="img/icono-menu.png" style="width: 25px; height: 25px">
+      		</button> 
+      		<a href="sucursales.php" class="navbar-brand"><label style="color: white; font-family: 'Trebuchet MS'">Inicio</label></a> 
+    	</div> 
+    	<nav class="navbar-collapse collapse" id="bs-navbar" aria-expanded="false" style="height: 1px;"> 
+      		<ul class="nav navbar-nav"> 
+        		<li class="texto-contenedor-1">
+	            	<a href="index.php">Volver</a>
+	          	</li>
+      		</ul> 
+    	</nav> 
+  	  </div> 
+    </header>   
+	
+
+	      <div>
+				<input type="search" id="buscar" placeholder="Buscar" class="form-control input-lg input-search ">
+	      </div>
+	      <br>
+
+	  <div style="margin-left: 25%" class="table-responsive container">
+	  	<div class="row">
+  		<div class="col-sm-6">
+ 
+	   <div class="ih-item circle effect13 from_left_and_right"><a href="#">
+        <div class="img"><img src="img/carne.png" alt="img"></div>
+        <div class="info">
+          <div class="info-back">
+            <h3>Carnes</h3>
+            <p>Los cortes más frescos</p>
+          </div>
+        </div></a></div>
+ 
+  </div>
+
+  <div class="col-sm-6">
+ 
+     <div class="ih-item circle effect13 from_left_and_right"><a href="#">
+        <div class="img"><img src="img/bebidas.png" alt="img"></div>
+        <div class="info">
+          <div class="info-back">
+            <h3>Bebidas</h3>
+            <p>Variedad de bebidas a tu alcance</p>
+          </div>
+        </div></a></div>
+ 
+  </div>
+  
+  <div class="col-sm-6">
+ 
+    <div class="ih-item circle colored effect13 from_left_and_right"><a href="#">
+        <div class="img"><img src="img/snacks.png" alt="img"></div>
+        <div class="info">
+          <div class="info-back">
+            <h3>Snacks</h3>
+            <p>Para todos los gustos</p>
+          </div>
+        </div></a></div>
+ 
+  </div>
+
+   <div class="col-sm-6">
+ 
+    <div class="ih-item circle colored effect13 from_left_and_right"><a href="#">
+        <div class="img"><img src="img/vegetales.png" alt="img"></div>
+        <div class="info">
+          <div class="info-back">
+            <h3>Vegetales</h3>
+            <p>Los vegetales mas frescos, del campo a su mesa</p>
+          </div>
+        </div></a></div>
+ 
+  </div>  
+</div>
+ 
+ 
+<!-- Top to bottom -->
+<div class="row">
+  <div class="col-sm-6">
+ 
+    <!-- normal -->
+    <div class="ih-item circle effect13 top_to_bottom"><a href="#">
+        <div class="img"><img src="img/cocina.png" alt="img"></div>
+        <div class="info">
+          <div class="info-back">
+            <h3>Cocina</h3>
+            <p>Todo lo que necesitas para ser un chef</p>
+          </div>
+        </div></a></div>
+ 
+  </div>
+  <div class="col-sm-6">
+     <div class="ih-item circle colored effect13 top_to_bottom"><a href="#">
+        <div class="img"><img src="img/condimentos.png" alt="img"></div>
+        <div class="info">
+          <div class="info-back">
+            <h3>Condimentos</h3>
+            <p>Finas especias para darle sabor a tus comidas</p>
+          </div>
+        </div></a>
+     </div> 
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-sm-6">
+    <div class="ih-item circle effect13 bottom_to_top"><a href="#">
+        <div class="img"><img src="img/higiene.png" alt="img"></div>
+        <div class="info">
+          <div class="info-back">
+            <h3>Higiene</h3>
+            <p>Todo lo que necesitas para tu cuidado personal</p>
+          </div>
+        </div></a></div>
+ 
+  </div>
+  <div class="col-sm-6">
+ 
+    <!-- colored -->
+    <div class="ih-item circle colored effect13 bottom_to_top"><a href="#">
+        <div class="img"><img src="img/lacteos.png" alt="img"></div>
+        <div class="info">
+          <div class="info-back">
+            <h3>Lacteos</h3>
+            <p>Deliciosos lacteos</p>
+          </div>
+        </div></a></div>
+    <!-- end colored -->
+ 
+  </div>
+  
+  <div class="col-sm-6">
+    <div class="ih-item circle effect13 bottom_to_top"><a href="#">
+        <div class="img"><img src="img/detergentes.png" alt="img"></div>
+        <div class="info">
+          <div class="info-back">
+            <h3>Detergentes</h3>
+            <p>Variedad de marcas para cumplir con tus necesidades</p>
+          </div>
+        </div></a></div>
+ 
+  </div>
+
+
+  <div class="col-sm-6">
+    <div class="ih-item circle effect13 bottom_to_top"><a href="#">
+        <div class="img"><img src="img/papeleria.png" alt="img"></div>
+        <div class="info">
+          <div class="info-back">
+            <h3>Papeleria</h3>
+            <p>Variedad de utiles para tu oficina</p>
+          </div>
+        </div></a></div>
+ 
+  </div>  
+
+  <div class="col-sm-6">
+ 
+    <div class="ih-item circle colored effect13 from_left_and_right"><a href="#">
+        <div class="img"><img src="img/jugueteria.png" alt="img"></div>
+        <div class="info">
+          <div class="info-back">
+            <h3>Jugueteria</h3>
+            <p>Gran variedad de juguetes para todas las edades</p>
+          </div>
+        </div></a></div>
+ 
+  </div>  
+
+  <div class="col-sm-6">
+ 
+    <div class="ih-item circle colored effect13 from_left_and_right"><a href="#">
+        <div class="img"><img src="img/panaderia.png" alt="img"></div>
+        <div class="info">
+          <div class="info-back">
+            <h3>Panaderia</h3>
+            <p>Exquisitos panes para satisfacer tu paladar</p>
+          </div>
+        </div></a></div>
+ 
+  </div>
+
+  <div class="col-sm-6">
+ 
+    <div class="ih-item circle colored effect13 from_left_and_right"><a href="#">
+        <div class="img"><img src="img/congelados.png" alt="img"></div>
+        <div class="info">
+          <div class="info-back">
+            <h3>Congelados</h3>
+            <p>Listos para calentar y disfrutar</p>
+          </div>
+        </div></a></div>
+ 
+  </div>    
+
+  <div class="col-sm-6">
+ 
+    <div class="ih-item circle colored effect13 from_left_and_right"><a href="#">
+        <div class="img"><img src="img/mascotas.png" alt="img"></div>
+        <div class="info">
+          <div class="info-back">
+            <h3>Mascotas</h3>
+            <p>Todo para cuidar a tu mascota</p>
+          </div>
+        </div></a></div>
+ 
+  </div>    
+
+  <div class="col-sm-6">
+ 
+    <div class="ih-item circle colored effect13 from_left_and_right"><a href="#">
+        <div class="img"><img src="img/embutidos.png" alt="img"></div>
+        <div class="info">
+          <div class="info-back">
+            <h3>Embutidos</h3>
+            <p>Todo en alimentos procesados</p>
+          </div>
+        </div></a></div>
+ 
+  </div>    
+</div>
+<!-- end Bottom to top -->	
+	  </div>
+	   
+
+	      
+
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
--- visitantes.php
+++ visitantes.php
@@ -1,7 +1,3 @@
-<?php session_start(); 
-  if(!isset($_SESSION['codigo_usuario']))
-    header("Location: index.php");
-?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -20,38 +16,114 @@
       <div>
         <img  class="logo" src="img/titulo4.png" align="center" width="500px" height="100px" hspace="450px">
       </div>
-      <nav class="menu">
+      <nav class="menu nav">
         <ul class="barra_superior">     
           <li class="elemento_izquierda active">
-            <a href="">Inicio</a>
+            <a href="index.php">Inicio</a>
           </li>
           <li class="elemento_izquierda">
-            <a href="">Sucursales</a>
-            
+            <a href="vista_sucursales.php">Sucursales</a>
           </li>
           <li class="elemento_izquierda">
-            <a href="#">Productos</a>
+            <a href="productos.php">Productos</a>
           </li>
           <li class="elemento_izquierda">
-            <a href="#">Contactenos</a>
+            <a href="#" data-toggle="modal" data-target="#myModal">Contactenos</a>
+            <div id="myModal" class="modal fade" tabindex="-1" role="dialog">
+              <div class="modal-dialog" role="document" style="color: black">
+                <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                      <h4 class="modal-title">Porque nos interesa mejorar para ti</h4>
+                    </div>
+                      <div class="modal-body">
+                        <p>Encuentranos en nuestro Facebook como SupermerPOO o contactanos a nuestro correo supermerPOO@gmail.com
+                        </p>
+                      </div>
+                      <div class="modal-footer">
+                      <button type="button" class="btn btn-primary" data-dismiss="modal">Cerrar</button>
+                      </div>
+                </div>
+              </div>
+          </div>
           </li>
           <li class="elemento_izquierda">
-            <a href="#">Acerca de</a>
+            <a href="#" data-toggle="modal" data-target="#myModal2">Acerca de</a>
+            <div id="myModal2" class="modal fade" tabindex="-1" role="dialog" style="position: center">
+              <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                      <h4 class="modal-title">Quienes somos?</h4>
+                    </div>
+                      <div class="modal-body">
+                        <p>Somos una cadena de supermercados en la cual nuestro enfoque es ofrecer a nuestro clientes la mejor calidad de productos al mejor precio, dando una atencion calida, como si estuvieras en casa.
+                        </p>
+                      </div>
+                      <div class="modal-footer">
+                      <button type="button" class="btn btn-primary" data-dismiss="modal">Cerrar</button>
+                      </div>
+                </div>
+              </div>
+          </div>
           </li>
           
           <li class="elemento_derecha">
-            <a href="registro_visitantes.php">Registrarse</a>
+            <a href="registro_usuarios.php">Registrarse</a>
           </li>
 
           <li class="elemento_derecha">
-            <a href="index.php">Iniciar Sesión</a>
+            <a href="login.php">Iniciar Sesión</a>
           </li>
           </div>
         </ul>
       </nav>
     </header>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <div align="center" id="myCarousel" class="carousel slide" data-ride="carousel">
     
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
+
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="img/animacion1.jpg" alt="Flower" style="width: 900px; height: 550px">
+        <div class="carousel-caption">
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="img/animacion2.jpg" alt="Flower" style="width: 900px; height: 550px">
+        <div class="carousel-caption">
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="img/animacion3.jpg" alt="Flower" style="width: 900px; height: 550px">
+        <div class="carousel-caption">
+        </div>
+      </div>
+
+      <div class="item">
+        <img src="img/animacion4.jpg" alt="Flower" style="width: 900px; height: 550px">
+        <div class="carousel-caption">
+        </div>
+      </div>
+    </div>
+
+    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
     <section>
        <div id="pie" class="piePagina">  
          <footer class="wrapper">
@@ -62,5 +134,9 @@
          </footer>
        </div>
   </section>
+
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/funciones.js"></script>
 </body>
 </html>
